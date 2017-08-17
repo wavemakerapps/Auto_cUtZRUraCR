059671c581f56385550b8f2159d64a115044cd34
QmLbNkUbWQ <automate1@wavemaker.com>
2017-08-17 18:24:09 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_cUtZRUraCR",
+  "homePage" : "Main",
+  "name" : "Auto_cUtZRUraCR",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
